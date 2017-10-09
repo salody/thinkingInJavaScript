a7bc66ac197c1c56fd36ad56e447cc8bd12713d3
liuguanbang <salodyliu@gmail.com>
2017-10-09 15:29:36 +0800

    block event loop

--- FunctionalProgramming/README.md
+++ FunctionalProgramming/README.md
@@ -8,5 +8,9 @@
 ├── demo05.js  鸭子模式，以及使用call来模仿对象行为
 ├── demo06.js  partical application 
 ├── demo07.js  判断target对象的某个方法method被调用了多少次.对某个函数进行中间件改造
-└── demo08.js  
+├── demo08.js  block event loop
+├── demo09.js  Trampolines & Tails(递归优化和尾递归)。可以看下面的文章
+├──            https://medium.com/@cukejianya/functional-js-trampolines-tails-88723b4da320
+└── demo08.js  block event loop
+
 ```
--- FunctionalProgramming/demo08.js
+++ FunctionalProgramming/demo08.js
@@ -1,6 +1,24 @@
 /**
- * 功能描述:
+ * 功能描述: block event loop
  * @author: liuguanbang
  * 2017/10/9
  */
 
+function repeat(operation, num) {
+  if (num <= 0) return;
+
+  operation();
+
+  // release control every 10 or so
+  // iterations.
+  // 10 is arbitrary.
+  if (num % 10 === 0) {
+    setTimeout(function() {
+      repeat(operation, --num)
+    })
+  } else {
+    repeat(operation, --num)
+  }
+}
+
+module.exports = repeat;
